0c7def9219ae4732a60c4184ddf5d9c533e79355
skywalerlajos <80207125+skywalerlajos@users.noreply.github.com>
2022-03-15 18:55:01 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    		//var ran = Math.floor(Math.random() * 100) + 1; 
-    		//alert(ran)
-    		alert("Your love is: " + ran + "% !");
-    	}
-
-    	var ran = Math.floor(Math.random() * 100) + 1; 
-    	//document.write(ran);
