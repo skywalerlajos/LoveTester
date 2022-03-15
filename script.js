a8ffd3f1ae54f49760cef86d3c1687476191e874
skywalerlajos <80207125+skywalerlajos@users.noreply.github.com>
2022-03-15 18:57:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function displayLovePercent() {
+    		//var ran = Math.floor(Math.random() * 100) + 1; 
+    		//alert(ran)
+    		alert("Your love is: " + random + "% !");
+    	}
+
+    	var random = Math.floor(Math.random() * 100) + 1; 
+    	//document.write(ran);
