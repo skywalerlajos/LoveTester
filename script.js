003b2f0b0896eeea15885d803c69ce9dcb1feaf1
skywalerlajos <80207125+skywalerlajos@users.noreply.github.com>
2022-03-15 18:47:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    		//var ran = Math.floor(Math.random() * 100) + 1; 
+    		//alert(ran)
+    		alert("Your love is: " + ran + "% !");
+    	}
+
+    	var ran = Math.floor(Math.random() * 100) + 1; 
+    	//document.write(ran);
